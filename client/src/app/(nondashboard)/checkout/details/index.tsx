9dd940198a04a0144481ba6c0c8dab8bdd08f946
Eugene <yakubovskyeugene@gmail.com>
2025-06-24 02:06:33 +0300

    Guest items center 2

--- client/src/app/(nondashboard)/checkout/details/index.tsx
+++ client/src/app/(nondashboard)/checkout/details/index.tsx
@@ -38,7 +38,7 @@ const CheckoutDetailsPage = () => {
             flex items-center justify-center
           "
         >
-          <div className="w-full">
+          <div className="w-full flex flex-col items-center"> {/* Центрируем содержимое */}
             {showSignUp ? <SignUpComponent /> : <SignInComponent />}
           </div>
         </div>
